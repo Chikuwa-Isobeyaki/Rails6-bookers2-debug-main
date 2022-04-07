4a71aeffe6d5894caf15ae75877434fd369cb699
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2022-04-07 09:53:23 +0000

    いいね機能とコメント機能を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user =@book.user
+    @book_comment = BookComment.new
   end
 
   def index
@@ -15,7 +16,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
+      redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
       @books = Book.all
       render :index
@@ -46,9 +47,5 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
-  private
-
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
+  
 end
---
+++ db/migrate/20220407051540_add_book_image_id_to_book_comment.rb
@@ -0,0 +1,5 @@
+class AddBookImageIdToBookComment < ActiveRecord::Migration[6.1]
+  def change
+    add_column :book_comments, :book_id, :integer
+  end
+end

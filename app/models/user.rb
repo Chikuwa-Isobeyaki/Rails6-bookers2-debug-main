9cd6c77fe2c47a04850367f6447f1f4b5fa9666b
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2022-04-09 09:29:41 +0000

    フォロワー機能作成中（モデル、コントローラー完了）

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,32 @@ class User < ApplicationRecord
   has_many :favorites,  dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :follower, source: :followed
+  has_many :followers, through: :followed, source: :follower
+  
   has_one_attached :profile_image
   
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50} 
 
+
+# フォロワー関係
+  def follow(user_id)
+    follower.create(folloewd_id: user_id)
+  end
   
+  def unfollow(user_id)
+    follower.find(followed_id: user_id).destroy
+  end
   
+  def following?(user_id)
+    following_user.include?(user)
+  end
+  
+# 　プロフィール画像取得
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end

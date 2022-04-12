118cb5ac03b0e01fb171a8f2c37fb171f5c837a6
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2022-04-12 01:36:35 +0000

    コメント、いいね機能の非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,17 @@
 class BookCommentsController < ApplicationController
-  
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @comment = current_user.book_comments.new(post_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
   
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
   end
   
   private
-  
   def post_comment_params
     params.require(:book_comment).permit(:comment)
   end
